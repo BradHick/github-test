a71a0bc4b921b13bb22708133aad6861bf3a6edc
BradHick <bradleyhrm@gmail.com>
2019-01-17 22:51:58 -0300

    feat: create user model

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+import { client } from '../client/client';
+
+const url = '/users';
+
+const user = {
+
+  state: {
+    user: {
+      login: '',
+      id: '',
+      node_id: '',
+      avatar_url: '',
+      gravatar_id: '',
+      url: '',
+      html_url: '',
+      followers_url: '',
+      following_url: '',
+      gists_url: '',
+      starred_url: '',
+      subscriptions_url: '',
+      organizations_url: '',
+      repos_url: '',
+      events_url: '',
+      received_events_url: '',
+      type: '',
+      site_admin: '',
+      name: '',
+      company: '',
+      blog: '',
+      location: '',
+      email: '',
+      hireable: '',
+      bio: '',
+      public_repos: '',
+      public_gists: '',
+      followers: '',
+      following: '',
+      created_at: '',
+      updated_at: ''
+    },
+    repos: [],
+    errors: {}
+  },
+}
