875495d99b6fcf066f57be53aaee8be7610ca52c
BradHick <bradleyhrm@gmail.com>
2019-01-21 22:50:06 -0300

    refactor: create reducers and effects to users model

--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,90 @@
 import { client } from '../client/client';
+import Immutable from 'seamless-immutable';
+
 
 const url = '/users';
 
+//user schema
+//     login: '',
+//     id: '',
+//     node_id: '',
+//     avatar_url: '',
+//     gravatar_id: '',
+//     url: '',
+//     html_url: '',
+//     followers_url: '',
+//     following_url: '',
+//     gists_url: '',
+//     starred_url: '',
+//     subscriptions_url: '',
+//     organizations_url: '',
+//     repos_url: '',
+//     events_url: '',
+//     received_events_url: '',
+//     type: '',
+//     site_admin: '',
+//     name: '',
+//     company: '',
+//     blog: '',
+//     location: '',
+//     email: '',
+//     hireable: '',
+//     bio: '',
+//     public_repos: '',
+//     public_gists: '',
+//     followers: '',
+//     following: '',
+//     created_at: '',
+//     updated_at: ''
+
+const state = new Immutable( {
+  user: {},
+  loading: false,
+  errors: {}
+} );
+
 const user = {
 
-  state: {
-    user: {
-      login: '',
-      id: '',
-      node_id: '',
-      avatar_url: '',
-      gravatar_id: '',
-      url: '',
-      html_url: '',
-      followers_url: '',
-      following_url: '',
-      gists_url: '',
-      starred_url: '',
-      subscriptions_url: '',
-      organizations_url: '',
-      repos_url: '',
-      events_url: '',
-      received_events_url: '',
-      type: '',
-      site_admin: '',
-      name: '',
-      company: '',
-      blog: '',
-      location: '',
-      email: '',
-      hireable: '',
-      bio: '',
-      public_repos: '',
-      public_gists: '',
-      followers: '',
-      following: '',
-      created_at: '',
-      updated_at: ''
+  state,
+
+  reducers: {
+    fetchUserFulfiled: ( state, payload ) => {
+      return state.merge({ user: payload, loading: false });
     },
-    repos: [],
-    errors: {}
-  },
-}
+
+    fetchUserPending: (state) => {
+      return state.merge({ loading: true });
+    },
+
+    fetchUserRejected: (state, payload) => {
+      return state.merge({ errors: payload.errors, loading: false });
+    },
+
+    resetUser: (state) => {
+      return state.merge({
+        user: {},
+        loading: false,
+        errors: {}
+      });
+    },
+
+    effcts: (dispatch) => ({
+      fetchUser(name){
+        dispatch.contact.fetchUserPending();
+        return client.get(`${url}/${name}`)
+          .then(res => {
+            dispatch.contact.fetchUserFulfiled(res.data);
+          });
+      },
+    })
+
+
+
+
+  }
+
+
+};
+
+
+export default user;
