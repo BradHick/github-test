2738f0156542b4e07c50fdc2a35242883380ff0e
BradHick <bradleyhrm@gmail.com>
2019-01-23 23:43:40 -0300

    fix typo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store/index';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
