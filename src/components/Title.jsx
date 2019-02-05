a35b56cb336207bf66c988889b58dc109ce5f1ec
BradHick <bradleyhrm@gmail.com>
2019-02-04 23:11:22 -0300

    feat: Create Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 36px;
+  color: #333363;
+  font-family: Montserrat;
+  font-weight: 400;
+  margin-bottom: 48px;
+  letter-spacing: 2px;
+  text-align: center;
+  text-transform: uppercase;
+  line-height: 45px;
+`;
+
+export default Title;
