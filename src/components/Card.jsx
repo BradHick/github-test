1eaad0c139062d08b899a53c03be32e30704bcc8
BradHick <bradleyhrm@gmail.com>
2019-02-04 23:09:27 -0300

    feat: Create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  width: 40%;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.08);
+  background-color: #ffffff;
+  min-height: 40px;
+  margin: 0 auto;
+  padding: 20px;
+  margin-top: 10px;
+  text-decoration: none;
+  display: flex;
+  @media (max-width: 800px) { width: 80%; }
+`;
+
+export default Card;
