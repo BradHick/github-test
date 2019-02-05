9a0a33a00e120509f37b8c2fb69744d64e922bc6
BradHick <bradleyhrm@gmail.com>
2019-02-04 23:11:54 -0300

    feat: export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,25 @@
 import Header from './Header';
 import Footer from './Footer';
+import Container from './Container';
+import Avatar from './Avatar';
+import Input from './Input';
+import Loading from './Loading';
+import Card from './Card';
+import Title from './Title';
+import NotFound from './NotFound';
+import UsersList from './UsersList';
+import If from './If';
 
 export {
   Header,
-  Footer
+  Footer,
+  Container,
+  Avatar,
+  Input,
+  If,
+  Loading,
+  Card,
+  Title,
+  NotFound,
+  UsersList
 };
