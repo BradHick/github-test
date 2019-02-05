c9163db005cb1d1ceb19df020f706dcd8d2f3ae4
BradHick <bradleyhrm@gmail.com>
2019-02-04 23:10:49 -0300

    feat: Create Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const InputContainer = styled.div`
+  position: relative;
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  height: 33px;
+  background-color: rgba(255,188,53,0);
+  border: 1px solid  #e9e9e9;
+  text-align: center;
+  font-family: Montserrat;
+  letter-spacing: 1px;
+`;
+
+const Icon = styled.i`
+  position: absolute;
+  right: 8px;
+  color: #606060;
+  top: 12px;
+`;
+
+const Input = ({ ...props }) => (
+  <InputContainer>
+    <InputField {...props} />
+    <Icon className='fas fa-search' />
+  </InputContainer>
+);
+
+export default Input;
