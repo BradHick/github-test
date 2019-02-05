e88cbd5c78ab965bd1571209ff3e3f0bea14544f
BradHick <bradleyhrm@gmail.com>
2019-02-05 17:27:10 -0300

    feat: create Info component

---
+++ src/components/Info.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  text-align: center;
+`;
+
+const Description = styled.span`
+  font-family: Roboto;
+  font-size: 14px;
+  letter-spacing: 1px;
+  margin-top: 8px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #afb2b4;
+`;
+
+const Title = styled.h1`
+  font-family: Montserrat;
+  font-size: 50px;
+  color: #d7d8d9;
+`;
+
+const Info = ({ description, value }) => (
+  <InfoContainer>
+    <Title> {value} </Title>
+    <Description> {description} </Description>
+  </InfoContainer>
+);
+
+export default Info;
