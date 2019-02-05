a0fc676f5ff830f81f19c3090d0e36de1c048103
BradHick <bradleyhrm@gmail.com>
2019-02-04 23:10:57 -0300

    feat: Create Loading Component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 0px;
+`;
+
+export default Loading;
