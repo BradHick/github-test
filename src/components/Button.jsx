068015e30431668d0bef3c4b6dac5d917422a1f1
BradHick <bradleyhrm@gmail.com>
2019-02-05 23:48:56 -0300

    feat: create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 43.5%;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #29b6f6;
+  border: none;
+  margin: 10px auto;
+  display: block;
+  color: #fff;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-family: Roboto;
+`;
+
+export default Button;
