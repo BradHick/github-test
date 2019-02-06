5e335eb592530720c55d7462d196c7db19754bc0
BradHick <bradleyhrm@gmail.com>
2019-02-06 09:59:39 -0300

    refactor(Commit): add a empty message

--- src/Scenes/Commit/index.js
+++ src/Scenes/Commit/index.js
@@ -64,11 +64,20 @@ class CommitList extends Component {
                   username={commit.author.name}
                   message={commit.message}
                   date={moment(commit.committer.date).format('DD/MM/YYYY')}
-                  img={author.avatar_url}
+                  img={author ? author.avatar_url : 'https://camo.githubusercontent.com/17dfc9f3bbe544abb10021fbe62a3640fe935b23/68747470733a2f2f302e67726176617461722e636f6d2f6176617461722f39363230333535376238396136386563653331623238306666373366303835613f643d68747470732533412532462532466769746875622e6769746875626173736574732e636f6d253246696d6167657325324667726176617461727325324667726176617461722d757365722d3432302e706e6726723d6726733d3430'}
                 />
               </Card>
             );
           })}
+        </If>
+        <If condition={commitsList.length <= 0}>
+          <Card>
+            <Commit
+              username={'Empty :('}
+              message={'This repository has no commits'}
+              img={'https://camo.githubusercontent.com/17dfc9f3bbe544abb10021fbe62a3640fe935b23/68747470733a2f2f302e67726176617461722e636f6d2f6176617461722f39363230333535376238396136386563653331623238306666373366303835613f643d68747470732533412532462532466769746875622e6769746875626173736574732e636f6d253246696d6167657325324667726176617461727325324667726176617461722d757365722d3432302e706e6726723d6726733d3430'}
+            />
+          </Card>
         </If>
         <If condition={loading}>
           <Loading>
